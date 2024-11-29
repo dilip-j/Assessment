a3e8adbbe64b7e08b282ab6def5800e4d8856332
dilip-j <dilipjadam25@gmail.com>
2024-11-29 16:02:23 +0530

    StringCalculator

---
+++ README.md
@@ -0,0 +1,31 @@
+# String Calculator
+
+A simple string calculator built in JavaScript using Test-Driven Development (TDD).
+
+## Features
+- Handles empty strings.
+- Adds single or multiple numbers (comma-separated).
+- Supports new line as a delimiter.
+- Allows custom delimiters.
+- Throws exceptions for negative numbers.
+
+## Requirements
+- Node.js
+- npm
+
+## Installation
+
+1. Clone the repository:
+
+## install
+1. npm install
+
+## run the file
+1. comment out line no. 21 in stringCalculator.js and uncomment line no 20 
+
+2. node stringCalculator.js
+
+
+## Run tests with Jest:
+1. npm test
+
---
+++ stringCalculator.js
@@ -0,0 +1,22 @@
+function add(numbers) {
+    if (numbers === "") return 0;
+  
+    let delimiter = /,|\n/; // Default delimiters
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n", 2);
+      delimiter = new RegExp(parts[0].slice(2)); // Custom delimiter
+      numbers = parts[1]; // Exclude the delimiter line
+    }
+  
+    const numArray = numbers.split(delimiter).map(Number);
+  
+    const negatives = numArray.filter(num => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+  
+    return numArray.reduce((sum, num) => sum + num, 0);
+  }
+//   console.log(add("1,2,3,7")); // Output: 6
+  module.exports = add;
+  
