6acdbdc2736171584dde57410bc768695eee7e2c
dilip-j <dilipjadam25@gmail.com>
2024-12-10 17:23:25 +0530

    wrtie a test case and method  for when we pass 0 a number

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,19 +1,5 @@
 function add(numbers) {
-    if (numbers === "") return 0;
-  
-    let delimiter = /,|\n/; // Default delimiters
-    if (numbers.startsWith("//")) {
-      const parts = numbers.split("\n", 2);
-      delimiter = new RegExp(parts[0].slice(2)); // Custom delimiter
-      numbers = parts[1]; // Exclude the delimiter line
-    }
-  
-    const numArray = numbers.split(delimiter).map(Number);
-  
-    const negatives = numArray.filter(num => num < 0);
-    if (negatives.length > 0) {
-      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
-    }
+    if (numbers === "") return 0; 
   
     return numArray.reduce((sum, num) => sum + num, 0);
   }
